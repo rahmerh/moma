dd7df7bc99f6e1a90233e5ba2e0e48b45fc82b1b
Bas Bouwhuis <basbouwhuis@protonmail.com>
2025-06-17 20:00:13 +0200

    Small tweaks to launch, almost done now

--- src/commands/launch.rs
+++ src/commands/launch.rs
@@ -23,7 +23,7 @@ pub struct Launch {
 impl Launch {
     pub fn run(&self, config: &mut Config) -> anyhow::Result<()> {
         if !os::is_process_root() {
-            bail!("Must be run as root.");
+            bail!("This command must be run as root (UID 0). Try again with `sudo`.");
         }
 
         let steam_dir = config.get_steam_dir()?;
@@ -32,14 +32,11 @@ impl Launch {
         if !context.validate_sink_is_empty()? {
             if self.force {
                 println!(
-                    "{} {}You are running {} with a non-empty sink folder.",
+                    "{} You are running {} with a non-empty sink folder.\n{}",
                     "Warning!".red().bold().underline(),
-                    "".white(),
-                    self.game_name.underline().bold()
+                    self.game_name.underline().bold(),
+                    "To prevent unexpected overwrites, move everything into appropriate mod folders.".yellow()
                 );
-                println!("{}",
-                    "To prevent unexpected overwrites, make sure to move everything into appropriate mod folders.".yellow()
-                )
             } else {
                 bail!(
                     "Your sink folder '{}' is not empty. Move this to an appropriate mod folder or add the force flag to continue.",
@@ -66,6 +63,13 @@ impl Launch {
 
         os::drop_privileges()?;
 
+        if !context.proton_binary().exists() {
+            bail!(
+                "Proton binary not found at '{}'",
+                context.proton_binary().display()
+            );
+        }
+
         let mut proton_cmd = Command::new(context.proton_binary());
         proton_cmd.current_dir(&context.active_dir());
         proton_cmd.envs(std::env::vars());
@@ -77,7 +81,9 @@ impl Launch {
                 .active_dir()
                 .join(&context.profile.game_mod_executable()),
         );
-        proton_cmd.spawn()?;
+        proton_cmd
+            .spawn()
+            .with_context(|| "Failed to start Proton process")?;
 
         Ok(())
     }
