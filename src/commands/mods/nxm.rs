4259733f2aa19200fb120d9d5fa7fc89c500f130
Bas Bouwhuis <basbouwhuis@protonmail.com>
2025-06-23 23:33:18 +0200

    Don't redownload cached mod files

--- src/commands/mods/nxm.rs
+++ src/commands/mods/nxm.rs
@@ -1,5 +1,6 @@
 use std::process::Command;
 
+use anyhow::Context;
 use clap::Args;
 
 use crate::{
@@ -24,35 +25,40 @@ impl NxmHandler {
         let game_config = config.games.get(game.id()).ok_or_else(|| {
             anyhow::anyhow!("No configuration found for game {}", game.to_string())
         })?;
+        let workspace = Workspace::new(config, game_config)?;
 
         let mod_info = Nexus::get_mod_info(&game, &parsed.mod_id).await?;
         let file_info = Nexus::get_mod_file_info(&game, &parsed.mod_id, &parsed.file_id).await?;
 
-        Command::new("notify-send")
-            .arg(&format!("Starting mod download for {}", mod_info.name))
-            .spawn()?;
+        let mod_cache_base_dir = &workspace.cache_dir().join(&mod_info.name);
+        let output_path = mod_cache_base_dir.join(&file_info.file_name);
 
-        let download_link = Nexus::get_download_link(parsed).await?;
+        if !output_path.exists() {
+            std::fs::create_dir(mod_cache_base_dir).with_context(|| {
+                format!("Folder '{}' already exists.", mod_cache_base_dir.display())
+            })?;
 
-        let workspace = Workspace::new(config, game_config)?;
+            Command::new("notify-send")
+                .arg(&format!("Starting mod download for {}", mod_info.name))
+                .spawn()?;
 
-        let mod_base_dir = &workspace.cache_dir().join(&mod_info.name);
-        std::fs::create_dir(mod_base_dir)?;
-        let output_path = mod_base_dir.join(&file_info.file_name);
-        Nexus::download_file(&download_link, &output_path).await?;
+            let download_link = Nexus::get_download_link(parsed).await?;
 
-        Command::new("notify-send")
-            .arg(format!(
-                "Downloaded {} to '{}'",
-                mod_info.name,
-                workspace.mods_dir().join(&file_info.file_name).display()
-            ))
-            .spawn()?;
+            Nexus::download_file(&download_link, &output_path).await?;
 
-        let extracted_path = &mod_base_dir.join(&mod_info.name).expand();
-        println!("{}", extracted_path.display());
-        std::fs::create_dir(extracted_path)?;
-        fs::extract_archive(&output_path, &extracted_path, true)?;
+            Command::new("notify-send")
+                .arg(format!(
+                    "Downloaded {} to '{}'",
+                    mod_info.name,
+                    workspace.mods_dir().join(&file_info.file_name).display()
+                ))
+                .spawn()?;
+
+            let extracted_path = &mod_cache_base_dir.join(&mod_info.name).expand();
+            println!("{}", extracted_path.display());
+            std::fs::create_dir(extracted_path)?;
+            fs::extract_archive(&output_path, &extracted_path, false)?;
+        }
 
         Ok(())
     }
