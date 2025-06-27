0c44eb567c286be55771a6cac8cf5364e9548384
Bas Bouwhuis <basbouwhuis@protonmail.com>
2025-06-28 01:53:39 +0200

    Macro tests

--- src/utils/macros.rs
+++ src/utils/macros.rs
@@ -7,9 +7,8 @@ macro_rules! usage_for {
 
         let mut cmd = _get_cmd();
         $(
-            cmd = cmd
-                .find_subcommand($path)
-                .unwrap_or_else(|| panic!("Unknown command path: {}", $path))
+            cmd = cmd.find_subcommand($path)
+                .expect(&format!("Unknown command path: {}", $path))
                 .clone();
         )+
         format!(
@@ -17,4 +16,33 @@ macro_rules! usage_for {
             cmd.render_usage().to_string().trim_start_matches("Usage:").trim()
         )
     }};
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::cli::Cli;
+
+    #[test]
+    fn usage_for_macro_should_return_correct_command_usage() {
+        // Act
+        let usage = usage_for!(Cli::INIT);
+
+        // Assert
+        assert_eq!(usage, "moma init");
+    }
+
+    #[test]
+    fn usage_for_macro_should_include_value_option_if_present() {
+        // Act
+        let usage = usage_for!(Cli::CONNECT);
+
+        // Assert
+        assert_eq!(usage, "moma connect <SOURCE>");
+    }
+
+    #[test]
+    #[should_panic]
+    fn usage_for_should_panic_if_unknown_command_given() {
+        let _ = usage_for!("invalid");
+    }
 }
